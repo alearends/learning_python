76d95fa96a756bc870a465637da78f56a7c3057f
alearends <alejandroarends77@gmail.com>
2025-04-11 15:33:35 +0200

    ejercicio del numero mayor con ternario

---
+++ 15_mayor_numero_ternario.py
@@ -0,0 +1,6 @@
+# El numero mayor de 2 dnumeros en ternario
+
+n_numero1 = float(input('Dame el primer numero a comparar: '))
+n_numero2 = float(input('Dame el segundo numero a comparar: '))
+
+n_mayor = print(f'El numero mayor es {n_numero1}') if n_numero1 > n_numero2 else print(f'el numero mayor es {n_numero2}')
