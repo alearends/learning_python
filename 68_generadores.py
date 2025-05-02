df7fc7e0f672e5a259ecaff23523aa1d1318ae1e
alearends <alejandroarends77@gmail.com>
2025-05-02 10:47:40 +0200

    generadores en python

--- 68_generadores.py
+++ 68_generadores.py
@@ -1,3 +1,4 @@
+'''
 def funcion_normal():
     return [1, 2, 3]  # Devuelve toda la lista de una vez
 
@@ -12,3 +13,26 @@ print(funcion_normal())  # Output: [1, 2, 3] (toda la lista en memoria)
 gen = generador()
 print(next(gen))  # Output: 1 (solo genera el primero)
 print(next(gen))  # Output: 2 (luego el segundo, manteniendo el estado)
+'''
+
+print('*** Generadores en Python ***')
+
+def generador(maximo):
+    contador = 0
+    while contador < maximo:
+        yield contador
+        contador += 1
+
+# Creamos un generador
+var_generador = generador(5)
+
+# iteramos sobre el generador
+for valor in var_generador:
+    print(valor)
+
+print('*** Expresiones Generadoras en Python ***')
+
+generador = (x**2 for x in range(10) if x % 2 == 0)
+
+for cuadrados_pares in generador:
+    print(cuadrados_pares)
